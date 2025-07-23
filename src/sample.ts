ab8f332a0f64adbdb0ca6cac1e17af444695f6d2
Lain-5OR4 <inazumahp@gmail.com>
2025-07-24 00:47:45 +0900

    update: 意図的なlinterrorの混入

--- src/sample.ts
+++ src/sample.ts
@@ -4,18 +4,16 @@ interface User {
 	email?: string;
 }
 
-// biome-ignore lint/correctness/noUnusedVariables: <explanationw>
 function formatUser(user: User): string {
 	const emailPart = user.email ? ` (${user.email})` : "";
 	return `${user.name}, ${user.age}${emailPart}`;
 }
 
-// biome-ignore lint/correctness/noUnusedVariables: <explanationw>
 const users: User[] = [
 	{ name: "Alice", age: 30, email: "alice@example.com" },
 	{ name: "Bob", age: 25 },
 ];
 
-// users.forEach(user => {
-//   console.log(formatUser(user));
-// });
+users.forEach((user) => {
+	console.log(formatUser(user));
+});
